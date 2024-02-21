7a7dad04ef205ea43597defd9bbd873e1d2f13f7
Fillipe Augusto <fillipe484@gmail.com>
2024-02-20 21:48:18 -0300

    chore: setup initial project structure for testing

---
+++ src/infra/infraTypes.ts
@@ -0,0 +1,5 @@
+export enum QueryKeys {
+  // This would reflect the query keys in the app based on the domain 
+  // Example:
+  // GetUsers = 'GetUsers',
+}
---
+++ src/test/test-utils.tsx
@@ -0,0 +1,64 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from '@shopify/restyle';
+import { QueryClient, QueryClientConfig, QueryClientProvider } from '@tanstack/react-query';
+import {
+  RenderOptions,
+  render,
+  renderHook,
+  RenderHookOptions,
+} from '@testing-library/react-native';
+import { theme } from '@theme';
+import { ReactElement } from 'react';
+
+const queryClientConfig: QueryClientConfig = {
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    // ✅ no more errors on the console for tests
+    //@ts-ignore
+    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+  },
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+};
+
+
+export const wrapAllProviders = () => {
+  const queryClient = new QueryClient(queryClientConfig);
+  return ({children}: {children: React.ReactNode}) => (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children} </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+function customRenderHook<Result, Props>(
+  renderCallback: (props: Props) => Result,
+  options?: Omit<RenderHookOptions<Props>, 'wrapper'>,
+) {
+  return renderHook(renderCallback, {
+    wrapper: wrapAllProviders(),
+    ...options,
+  });
+}
+
+function customRender<T = unknown>(
+  component: ReactElement<T>,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) {
+  return render(component, {wrapper: wrapAllProviders(), ...options});
+}
+
+export * from '@testing-library/react-native';
+export {customRender as render};
+export {customRenderHook as renderHook};
