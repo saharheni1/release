c1493ea8399c9c4457671978375e1d7c87cf06b7
Fillipe Augusto <fillipe484@gmail.com>
2024-02-20 21:30:17 -0300

    chore: setup theme config

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,40 +3,48 @@ import {ViewStyle} from 'react-native';
 import {createTheme} from '@shopify/restyle';
 
 export const palette = {
-  greenSuccess: '#00C853',
-  greenSuccessLight: '#E8F5E9',
-  redError: '#D50000',
-  redErrorLight: '#FFEBEE',
+  greenPrimary: '#074C4E',
+  greenPrimaryLight: '#EAF6F6',
+  carrotSecondary: '#F86F2D',
+  carrotSecondaryLight: '#FAE6DD',
+  greenSuccess: '#4ABC86',
+  greenSuccessLight: '#D8FFEC',
+  redError: '#EA3838',
+  redErrorLight: '#FBECEC',
 
   grayBlack: '#000000',
+  black60: 'rgba(0, 0, 0, 0.6)',
   gray1: '#636363',
   gray2: '#8E8E8E',
   gray3: '#B3B3B3',
   gray4: '#E1E1E1',
   gray5: '#F5F5F5',
   grayWhite: '#FFFFFF',
+  white70: 'rgba(255, 255, 255, 0.7)',
 };
 
 export const theme = createTheme({
   colors: {
     ...palette,
-    // primary: palette.greenPrimary,
-    // primaryContrast: palette.grayWhite,
+    primary: palette.greenPrimary,
+    primaryContrast: palette.grayWhite,
 
-    // buttonPrimary: palette.greenPrimary,
+    buttonPrimary: palette.greenPrimary,
 
-    // background: palette.grayWhite,
-    // backgroundContrast: palette.grayBlack,
+    background: palette.grayWhite,
+    backgroundContrast: palette.grayBlack,
 
-    // error: palette.redError,
-    // errorLight: palette.redErrorLight,
+    error: palette.redError,
+    errorLight: palette.redErrorLight,
 
-    // success: palette.greenSuccess,
-    // successLight: palette.greenSuccessLight,
+    success: palette.greenSuccess,
+    successLight: palette.greenSuccessLight,
 
-    // marked: palette.carrotSecondary,
+    marked: palette.carrotSecondary,
   },
   spacing: {
+    s0: 0,
+    s2: 2,
     s4: 4,
     s8: 8,
     s10: 10,
@@ -49,20 +57,14 @@ export const theme = createTheme({
     s40: 40,
     s48: 48,
     s56: 56,
+    s64: 64,
   },
   borderRadii: {
     s4: 4,
     s8: 8,
     s10: 10,
     s12: 12,
-    s14: 14,
     s16: 16,
-    s20: 20,
-    s24: 24,
-    s32: 32,
-    s40: 40,
-    s48: 48,
-    s56: 56,
   },
   textVariants: {
     defaults: {},
